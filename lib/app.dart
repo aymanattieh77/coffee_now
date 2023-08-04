b67d61ecf709f8ce3e692dd46bad794b1e5a17da
ayman attieh <aymanattieh77@gmail.com>
2023-08-04 23:14:11 +0300

    refactor project structure #1

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,5 @@
+import 'package:coffee_now/config/routes/routes.dart';
+import 'package:coffee_now/config/theme/themes.dart';
 import 'package:flutter/material.dart';
 
 class CoffeeNow extends StatelessWidget {
@@ -6,12 +8,16 @@ class CoffeeNow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Coffee Now',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Scaffold(),
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.light,
+      // locale: ,
+      // supportedLocales: ,
+      // localizationsDelegates: ,
+      onGenerateRoute: RouterGenerator.onGenerateRoute,
+      // onUnknownRoute: (settings) {},
     );
   }
 }
---
+++ lib/config/routes/app_router.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  static MaterialPageRoute screen(Widget child) {
+    return MaterialPageRoute(builder: (ctx) => child);
+  }
+
+  static push(BuildContext context, Widget page, [bool replacement = false]) {
+    if (replacement) {
+      Navigator.of(context).pushReplacement(screen(page));
+    } else {
+      Navigator.of(context).push(screen(page));
+    }
+  }
+
+  static pushNamed(BuildContext context,
+      {required String routeName, bool replacement = false, Object? args}) {
+    if (replacement) {
+      Navigator.of(context).pushNamed(routeName, arguments: args);
+    } else {
+      Navigator.of(context).pushReplacementNamed(routeName, arguments: args);
+    }
+  }
+
+  static pop(BuildContext context) {
+    Navigator.of(context).pop();
+  }
+
+  static popUntil(BuildContext context, String routeName) {
+    Navigator.of(context).popUntil((route) => route.settings.name == routeName);
+  }
+}
