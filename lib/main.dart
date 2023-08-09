2ba251ea7588d5c3fea4fd05fc2a4153591f8035
ayman attieh <aymanattieh77@gmail.com>
2023-08-10 01:49:54 +0300

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:coffee_now/config/language/language_cubit.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:coffee_now/app.dart';
 import 'package:coffee_now/config/language/language_manager.dart';
 import 'package:coffee_now/config/services/service_locator.dart';
@@ -11,6 +12,9 @@ import 'package:flutter_phoenix/flutter_phoenix.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await EasyLocalization.ensureInitialized();
   await serviceLocatorStart();
   runApp(
